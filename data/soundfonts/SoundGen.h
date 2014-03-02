1cf93febb7f4a92dc43f0cbdad6282f1617b735e
Zachary Saraf <zsaraf@stanford.edu>
2014-03-01 17:47:42 -0800

    added soundgen

--- data/soundfonts/SoundGen.h
+++ data/soundfonts/SoundGen.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreAudio/CoreAudioTypes.h>
 
-@interface SoundGen : NSObject
+@interface SoundGen : NSObject <AVAudioSessionDelegate>
+
+-(id)initWithSoundFontURL:(NSURL *)soundFontURL patchNumber:(NSInteger)patchNumber;
+-(void)playMidiNote:(NSInteger)note velocity:(NSInteger)velocity;
+-(void)stopPlayingMidiNote:(NSInteger)note;
+
+@property (nonatomic, strong) NSURL *soundFontURL;
+@property (nonatomic) NSInteger patchNumber;
 
 @end
